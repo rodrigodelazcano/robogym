ed3b918d8d7cde8f2eeb93ee8edb2f78638751e9
Rodrigo Perez-Vicente <r.l.p.v96@gmail.com>
2022-03-30 22:53:03 -0400

    modigy setup

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ def setup_robogym():
         packages=find_packages(),
         install_requires=[
             # Fixed versions
-            "click==7.0",
+            "click>=7.0",
             "collision==1.2.2",
-            "gym==0.21.0",
+            "gym>=0.21.0",
             "kociemba==1.2.1",
             "mujoco-py<2.2,>=2.1",
             "pycuber==0.2.2",
-            "matplotlib==3.5.1",
+            "matplotlib<3.5.1,>=3.4.3",
             "transforms3d==0.3.1",
             # Minimum versions
             "jsonnet>=0.14.0",

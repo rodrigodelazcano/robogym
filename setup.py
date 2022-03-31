c972a5baaf83cdb975c23c07f15177a816bf8223
Rodrigo Perez-Vicente <r.l.p.v96@gmail.com>
2022-03-31 01:01:10 -0400

    remove installation requirements

--- setup.py
+++ setup.py
@@ -8,21 +8,6 @@ def setup_robogym():
         version=open("ROBOGYM_VERSION").read(),
         packages=find_packages(),
         install_requires=[
-            # Fixed versions
-            "click>=7.0",
-            "collision==1.2.2",
-            "gym>=0.21.0",
-            "kociemba==1.2.1",
-            "mujoco-py<2.2,>=2.1",
-            "pycuber==0.2.2",
-            "matplotlib<3.5.1,>=3.4.3",
-            "transforms3d==0.3.1",
-            # Minimum versions
-            "jsonnet>=0.14.0",
-            "pytest>=4.6.9",
-            "scikit-learn>=0.21.3",
-            "trimesh>=3.5.23",
-            "mock>=4.0.2",
         ],
         python_requires=">=3.7.4",
         description="OpenAI Robogym Robotics Environments",
